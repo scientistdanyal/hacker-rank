6e511f8a6a99cc4b0b63ea6e09c2ff44342da5ae
yalgar0 <dk.yalgar@gmail.com>
2023-07-31 12:45:50 +0500

    commit

---
+++ prob1.py
@@ -0,0 +1,16 @@
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+if n % 2 != 0 :
+    print('Weird')
+elif 2 <= n <= 5 and n % 2 == 0:    
+    print('Not Weird')
+elif 6 <= n <= 20 and n % 2 ==0:  
+    print('Weird')     
+else:
+    print('Not Weird')            
